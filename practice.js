2fe60722b3a34dfbea77f39e025a17956dfd2072
2azahar786 <2azahar786@gmail.com>
2023-02-06 03:42:44 +0530

    initial commit

---
+++ practice.js
@@ -0,0 +1,120 @@
+// function sum(){
+//     var x =11;
+//     var y = 9;
+//     console.log(x+y);
+// }
+
+// sum();
+
+// function sub(){
+//     var x = 15;
+//     var y = 20;
+//     console.log(x-y);
+// }
+
+// sub();
+
+// function sum (){
+//     var a = 20;
+
+//     function sub (parameter){
+//         function mult(){
+//         console.log(a);
+//         }
+//     }
+//     sub(a);
+//     }
+    
+// sum()
+
+
+// var sum = function(){
+//    var x = 10;
+//    var y = 15;
+//    return val = x+y;
+    
+// };
+// console.log(sum);
+
+// let sub = () =>{
+//     var x= 10;
+//     
+// }
+
+// console.log(sub);
+
+
+// HOf and Callback
+
+// settimeout
+
+// setTimeout(() => {
+//     console.log("Hello world");
+// }, 3000);
+
+// setInterval(() => {
+//     console.log("Hello friends");
+// }, 1000);
+
+// functional programming
+
+// foreach
+
+// const arr =["azhar","ansari","deepak","sumnat","ramjan","imran"]
+
+// arr.forEach((val) =>{
+//     console.log(val);
+// });
+
+// imp
+// Map, filter, reduce
+
+// const number = [1,2,3,4,5,6,7,8,9];
+// const num =number.map((val) =>{
+//     return val *val
+// });
+// console.log(num);
+
+// filter
+
+// const countries = [
+//     "Albania",
+//     "Bolivia",
+//     "Canada",
+//     "Denmark",
+//     "Ethiopia",
+//     "Finland",
+//     "Germany",
+//     "Hungary",
+//     "Ireland",
+//     "Indialand",
+//     "Japan",
+//     "Kenyaland",
+//     "pakistanland",
+//   ];
+
+//   const cntry = countries.filter((country) => country.includes("land"));
+//   console.log(cntry);
+
+// Reduce
+
+// const numbers = [
+//     25,45,55,78,56
+// ]
+
+// fruits.reduce(myfunc);
+
+// myfunc(total, num){
+//     return total -num;
+// };
+
+// console.log(numbers);
+
+// const numbers = [175, 50, 25];
+// const num = numbers.reduce(myFunc);
+
+// function myFunc(x, y) {
+//   return x - y;
+// }
+
+// console.log(num);
